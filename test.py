a1fec64aa82140ca1c85174ab4e6b57b7f018556
upnt <base.shun0329@gmail.com>
2021-04-09 01:05:42 +0900

    separate method

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-from stone import Stone, StoneState
+from stone import Stone, Color
 from board import RectBoard
 
 
-if __name__ == '__main__':
+def board_test():
     board = RectBoard(8, 8)
     board.add(1, 3, 3)
     board.add(1, 4, 4)
     board.add(2, 4, 3)
     board.add(2, 3, 4)
     board.draw()
+
+
+if __name__ == '__main__':
+    board_test()
