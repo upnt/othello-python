fb6ac3516a3ba67a9f44fad1a58ab08ead72901e
upnt <base.shun0329@gmail.com>
2021-04-09 16:29:07 +0900

    add license

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name="othello",
     version="1.0",
+    license="Apache License 2.0",
     description="You can play othello",
     author="upnt",
     url="https://github.com/upnt/othello-python",
